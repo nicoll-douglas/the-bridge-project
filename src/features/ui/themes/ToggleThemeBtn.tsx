6644794b99c3f7b9927cf41303ca6f0082b5af25
Nicoll Douglas <nicoll.leo.douglas@gmail.com>
2024-10-31 23:00:09 +0000

    style/refactor: edit style, update prop type

--- src/features/ui/themes/ToggleThemeBtn.tsx
+++ src/features/ui/themes/ToggleThemeBtn.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { IconButton, useColorMode } from "@chakra-ui/react";
+import { IconButton, IconButtonProps, useColorMode } from "@chakra-ui/react";
 
-export default function ToggleThemeBtn() {
+export default function ToggleThemeBtn(
+  props?: Omit<IconButtonProps, "aria-label">
+) {
   const { colorMode, setColorMode } = useColorMode();
   const isLight = colorMode === "light";
 
@@ -12,7 +14,8 @@ export default function ToggleThemeBtn() {
       aria-label="Toggle dark mode"
       icon={isLight ? <MoonIcon /> : <SunIcon />}
       onClick={() => setColorMode(isLight ? "dark" : "light")}
-      variant={"outline"}
+      variant={"ghost"}
+      {...props}
     />
   );
 }
